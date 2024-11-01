0c420e9c00f49ce874196863ea662bbbf53445d9
Christian David Batrina Jasmin <141408182+kyuremmmmz@users.noreply.github.com>
2024-11-01 15:51:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def calculate_trip():
     origin = data.get("origin")
     destination = data.get("destination")
     vehicle = data.get("vehicle");
-    response = model.generate_content(f"Using the vehicle of {vehicle} Calculate the hrs trip from {origin} to {destination} and calculate the price dont include any other text just estimate it and calculate the budget that will be spent even not accurate")
+    response = model.generate_content(f"Using the vehicle of {vehicle} Calculate the hrs trip from {origin} to {destination} and calculate the price include the place where ako sasakay na bus or jeep, and include google map link dont include any other text just estimate it and calculate the budget that will be spent even not accurate")
     return jsonify({"estimated_time": response.text})
     
 
